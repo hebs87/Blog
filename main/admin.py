9a15bdd14dbff8eba08e1249678a290838457579
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-09-11 00:44:19 +0100

    Added custom get_ordering method to BlogAdmin based on user type

--- main/admin.py
+++ main/admin.py
@@ -8,6 +8,15 @@ class BlogAdmin(admin.ModelAdmin):
     """ A custom BlogAdmin class to enable customising Blog admin view """
     list_display = ('title', 'date_created', 'last_modified', 'is_draft')
     list_filter = ('is_draft',)
+    # One way to order - this does it for all users
+    # ordering = ('title', '-date_created')
+
+    def get_ordering(self, request):
+        """ Override get_ordering method to customise ordering based on user type """
+        if request.user.is_superuser:
+            return 'title', '-date_created'
+
+        return 'title'
 
 
 admin.site.register(Blog, BlogAdmin)
