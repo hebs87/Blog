f216571bbf5c5487520bd33fb2ff5a28080c4213
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-09-11 00:01:51 +0100

    Customised main app verbose_name in admin panel

--- main/apps.py
+++ main/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+    # Customise app name in Django admin panel
+    verbose_name = 'blog management'
