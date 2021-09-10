852bd8e7cce1b4ec2bfe9b167b69d2b27d2ce9c0
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-09-10 23:55:18 +0100

    Created Blog model and ran migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+
+    title = models.CharField(max_length=255, blank=False)
+    body = models.TextField(max_length=3000)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    is_draft = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Blogs'
+
+    def __str__(self):
+        return self.title
