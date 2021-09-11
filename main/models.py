f379f60e80d9477595c31a18bb466d053cd50248
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-09-11 01:01:30 +0100

    Added custom save method to Blog model to populate slug when creating model if not present

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
 
-    slug = models.SlugField(max_length=100)
+    slug = models.SlugField(max_length=100, blank=False)
     title = models.CharField(max_length=255, blank=False)
     body = models.TextField(max_length=3000)
     date_created = models.DateTimeField(auto_now_add=True)
@@ -17,3 +17,12 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        """ Override save method to create slug if it is not present """
+        if not self.slug:
+            # Convert title to lower case, split words and join with a hyphen
+            title_words = self.title.lower().split(' ')
+            self.slug = '-'.join(title_words)
+
+        super(Blog, self).save(*args, **kwargs)
